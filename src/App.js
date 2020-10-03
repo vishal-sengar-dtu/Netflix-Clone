ba47e70df24b0eeafd01f6098bb0aa1e348d26e5
Vishal Sengar <vishalsengardtu@gmail.com>
2020-10-03 17:15:18 +0530

    Jumbotron Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from "react";
+import jumboData from "./fixtures/jumbo";
+import Jumbotron from "./components/jumbotron";
 
-function App() {
-  return <p>Hello</p>;
-}
+const App = () => {
+  return (
+    <Jumbotron.Container>
+      {jumboData.map((item) => {
+        return (
+          <Jumbotron key={item.id} direction={item.direction}>
+            <Jumbotron.Pane>
+              <Jumbotron.Title>{item.title}</Jumbotron.Title>
+              <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+            </Jumbotron.Pane>
+            <Jumbotron.Pane>
+              <Jumbotron.Image src={item.image} alt={item.alt} />
+            </Jumbotron.Pane>
+          </Jumbotron>
+        );
+      })}
+    </Jumbotron.Container>
+  );
+};
 
 export default App;
