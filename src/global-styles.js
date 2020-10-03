e340cc4820f4e0f11c2822bc35770ca53bc0c156
Vishal Sengar <vishalsengardtu@gmail.com>
2020-10-03 17:25:51 +0530

    Implementing Global Components

---
+++ src/global-styles.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+    html, body{
+        font-family: "Helvetica Neue", Arial, Helvetica, sans-serif;
+        background: black;
+        color: #333333;
+        font-size: 16px;
+    }
+`;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app";
+import { GlobalStyles } from "./global-styles";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <>
+    <GlobalStyles />
+    <App />
+  </>,
+  document.getElementById("root")
+);
