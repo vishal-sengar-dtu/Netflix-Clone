4b45097e8fa6f989fc9430bc9fb05242c45c5a44
Vishal Sengar <vishalsengardtu@gmail.com>
2020-10-04 17:27:10 +0530

    Accordion Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { JumbotronContainer } from "./containers/jumbotron";
+import { FaqsContainer } from "./containers/faqs";
 import { FooterContainer } from "./containers/footer";
 
 export default function App() {
   return (
     <>
       <JumbotronContainer />
+      <FaqsContainer />
       <FooterContainer />
     </>
   );
