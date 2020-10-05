0c6596d77c21691ef05413d0263a5cb2c14ab865
Vishal Sengar <vishalsengardtu@gmail.com>
2020-10-05 21:03:22 +0530

    Header Component

--- src/components/index.js
+++ src/components/index.js
@@ -5,3 +5,7 @@ export { default as Footer } from "./footer";
 export { default as Accordion } from "./accordion";
 
 export { default as OptForm } from "./opt-form";
+
+export { default as Header } from "./header";
+
+export { default as Feature } from "./feature";
