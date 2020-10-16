e4f97d36d1da1b3dd81aa673cf82a1fc9f52432a
Vishal Sengar <vishalsengardtu@gmail.com>
2020-10-16 19:31:41 +0530

    Header Profile

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,13 +1,45 @@
 import React from "react";
 import { Link as ReactRouterLink } from "react-router-dom";
-import { Background, Container, ButtonLink, Logo } from "./style/header";
+import { Background, Container, Feature, FeatureCallOut, Text, ButtonLink, Logo, Link, Group, Picture, Profile, Dropdown } from "./style/header";
 
 export default function Header({ bg = true, children, ...restProps }) {
   return bg ? <Background {...restProps}>{children}</Background> : children;
 }
 
+Header.Feature = function HeaderFeature({ children, ...restProps }) {
+  return <Feature {...restProps}>{children}</Feature>;
+};
+
+Header.FeatureCallOut = function HeaderFeatureCallOut({ children, ...restProps }) {
+  return <FeatureCallOut {...restProps}>{children}</FeatureCallOut>;
+};
+
+Header.TextLink = function HeaderTextLink({ children, ...restProps }) {
+  return <Link {...restProps}>{children}</Link>;
+};
+
+Header.Text = function HeaderText({ children, ...restProps }) {
+  return <Text {...restProps}>{children}</Text>;
+};
+
+Header.Dropdown = function HeaderDropdown({ children, ...restProps }) {
+  return <Dropdown {...restProps}>{children}</Dropdown>;
+};
+
+Header.Profile = function HeaderProfile({ children, ...restProps }) {
+  return <Profile {...restProps}>{children}</Profile>;
+};
+
+Header.Picture = function HeaderPicture({ src, ...restProps }) {
+  return <Picture {...restProps} src={`/images/users/${src}.png`} />;
+};
+
 Header.Frame = function HeaderFrame({ children, ...restProps }) {
   return <Container {...restProps}>{children}</Container>;
+};
+
+Header.Group = function HeaderGroup({ children, ...restProps }) {
+  return <Group {...restProps}>{children}</Group>;
 };
 
 Header.ButtonLink = function HeaderButtonLink({ children, ...restProps }) {
--- src/containers/browse.js
+++ src/containers/browse.js
@@ -2,6 +2,8 @@ import React,{useState, useContext, useEffect} from "react";
 import { SelectProfileContainer } from './profiles';
 import { FirebaseContext } from "../context/firebase";
 import { Header, Loading } from "../components";
+import * as ROUTES from "../constants/root";
+import logo from "../logo.svg"
 
 export function BrowseContainer({ slides }) {
   const [profile, setProfile] = useState({})
@@ -19,8 +21,36 @@ export function BrowseContainer({ slides }) {
     <>
       {loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody />}
 
-      <Header src="joker1">
-        <p>Hello World</p>
+      <Header src="joker1" dontShowOnSmallViewPort>
+        <Header.Frame>
+          <Header.Group>
+            <Header.Logo to={ROUTES.HOME} src={logo} alt="Netflix" />
+            <Header.TextLink>Series</Header.TextLink>
+            <Header.TextLink>Films</Header.TextLink>
+          </Header.Group>
+          <Header.Group>
+            <Header.Profile>
+              <Header.Picture src={user.photoURL} />
+              <Header.Dropdown>
+                <Header.Group>
+                  <Header.Picture src={user.photoURL} />
+                  <Header.TextLink>{user.displayName}</Header.TextLink>
+                </Header.Group>
+                <Header.Group>
+                  <Header.TextLink onClick={() => firebase.auth().signOut()}>Sign out</Header.TextLink>
+                </Header.Group>
+              </Header.Dropdown>
+            </Header.Profile>
+          </Header.Group>
+        </Header.Frame>
+        <Header.Feature>
+            <Header.FeatureCallOut>Watch Joker Now</Header.FeatureCallOut>
+            <Header.Text>
+              Forever alone in a crowd, failed comedian Arthur Fleck seeks connection as he walks the streets of Gotham
+              City. Arthur wears two masks -- the one he paints for his day job as a clown, and the guise he projects in a
+              futile attempt to feel like he's part of the world around him.
+            </Header.Text>
+          </Header.Feature>
       </Header>
     </>
   ) : (
