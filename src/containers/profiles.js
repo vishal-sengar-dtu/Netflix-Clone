c83f5711b6e5b9b15fe775122f37eab9ed6b343a
Vishal Sengar <vishalsengardtu@gmail.com>
2020-10-16 12:11:34 +0530

    Loading Profile Component

---
+++ src/containers/profiles.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Header, Profiles } from "../components";
+import * as ROUTES from "../constants/root";
+import logo from '../logo.svg';
+
+export function SelectProfileContainer({ user, setProfile}) {
+  return (
+    <>
+      <Header bg={false}>
+        <Header.Frame >
+          <Header.Logo to={ROUTES.HOME} src={logo} alt="Netflix"/>
+        </Header.Frame>
+      </Header>
+
+      <Profiles>
+        <Profiles.Title>Who's watching?</Profiles.Title>
+          <Profiles.List>
+          <Profiles.User onClick={() => {
+            setProfile({
+              displayName: user.displayName,
+              photoURL: user.photoURL,
+            })
+            }}>
+              <Profiles.Picture src={user.photoURL} />
+              <Profiles.Name>{user.dislayName}</Profiles.Name>
+            </Profiles.User> 
+          </Profiles.List>
+      </Profiles>
+    </>
+  );
+}
