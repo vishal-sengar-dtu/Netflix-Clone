e192497fda3c4037bccaef92a15051b1a5d73ab0
Vishal Sengar <vishalsengardtu@gmail.com>
2020-10-16 14:49:24 +0530

    Spinner component

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,7 +1,7 @@
 import React,{useState, useContext, useEffect} from "react";
 import { SelectProfileContainer } from './profiles';
 import { FirebaseContext } from "../context/firebase";
-import { Loading } from "../components";
+import { Header, Loading } from "../components";
 
 export function BrowseContainer({ slides }) {
   const [profile, setProfile] = useState({})
@@ -16,10 +16,14 @@ export function BrowseContainer({ slides }) {
   }, [profile.displayName])
 
   return profile.displayName ? (
-    loading ? (
-      <Loading src={user.photoURL} />
-    ) : null
+    <>
+      {loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody />}
+
+      <Header src="joker1">
+        <p>Hello World</p>
+      </Header>
+    </>
   ) : (
       <SelectProfileContainer user={user} setProfile={setProfile} />
     );
-}
+} 
